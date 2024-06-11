4781a26c34280e7e4d3122f76bc8e8386e5ccdec
VoHuynh <huynh.vo@unifiedpost.com>
2024-06-11 16:29:45 +0700

    feat: add performance testing scripts

--- ServerAnigan/model-evaluation.py
+++ ServerAnigan/model-evaluation.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from helpers.animegan import generate_anime_image
 
 source_img_path = "https://firebasestorage.googleapis.com/v0/b/ios-entertainment-photography.appspot.com/o/806187AA-DC94-40AD-BF52-8C20538B8A32-200102%2Fraw%2F1717311301.jpg?alt=media&token=64cab261-cf17-4e7d-9313-954b26f0ed87"
-num_tests = 10
+num_tests = 20
 response_times = []
 
 for _ in range(num_tests):
@@ -20,7 +20,10 @@ plt.xlabel('Times')
 plt.ylabel('Response Times (seconds)')
 plt.title('Model Response Times Over Multiple Runs')
 plt.grid(True)
+
+# Set y-axis to start from 0 seconds
+plt.ylim(0)
+
 plt.savefig('response_times_chart.png')
 plt.show()
-plt.savefig('response_times_chart.png')
 print("Chart saved as response_times_chart.png")
--- Testing/server-performance.py
+++ Testing/server-performance.py
@@ -41,13 +41,17 @@ def plot_response_times(times, response_times):
     plt.ylabel('Response Times (seconds)')
     plt.title('Endpoint Response Times Over Multiple Runs')
     plt.grid(True)
+
+    # Set y-axis to start from 0 seconds
+    plt.ylim(0)
+
     plt.savefig('response_times_chart.png')
     plt.show()
     print("Chart saved as response_times_chart.png")
 
 # Parameters
 source_img_url = "https://firebasestorage.googleapis.com/v0/b/ios-entertainment-photography.appspot.com/o/806187AA-DC94-40AD-BF52-8C20538B8A32-200102%2Fraw%2F1717311301.jpg?alt=media&token=64cab261-cf17-4e7d-9313-954b26f0ed87"
-num_tests = 10
+num_tests = 20
 
 # Run the tests and collect response times
 response_times = test_endpoint(source_img_url, num_tests)
